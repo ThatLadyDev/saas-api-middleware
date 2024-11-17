22453c06bf09bfa3b428a0a94780bfaf082cfd82
Lois Onyinyemme Bassey <45472714+ThatLadyDev@users.noreply.github.com>
2024-11-17 01:28:01 +0100

    configuration file containing list of services the api middleware can connect to with their corresponding URLs

---
+++ config/saas.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'notification-service' => env('NOTIFICATION_SERVICE_URL'),
+    'billing-service' => env('BILLING_SERVICE_URL'),
+    'authentication-service' => env('AUTHENTICATION_SERVICE_URL')
+];

f51d72175e63e06264bdeac4c257ae8fa792706c
Lois Onyinyemme Bassey <45472714+ThatLadyDev@users.noreply.github.com>
2024-11-17 01:29:15 +0100

    annotation addition to kickstart the generation of api documentation usingn swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,18 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="Devfest Lagos 2024 SAAS API Multi-Tenant Microservice",
+ *     description="API for a multi-tenant SaaS using microservices architecture on Kubernetes for Devfest Lagos 2024"
+ * )
+ *
+ * @OA\Server(
+ *     url="http://localhost:8090",
+ *     description="Local server"
+ * )
+*/
 abstract class Controller
 {
-    //
 }
